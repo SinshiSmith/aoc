41ca17d4584e43a62bbf7fa70e7db54243da19f4
SinshiSmith <yorfnd@gmail.com>
2022-12-03 13:45:26 +0200

    day-3: solve part-2

--- 2022/rust/day-3/src/lib.rs
+++ 2022/rust/day-3/src/lib.rs
@@ -1,11 +1,29 @@
+use itertools::Itertools;
+
+fn char_to_value(char: char) -> u32 {
+    let start_value = if char.is_lowercase() { 0 } else { 26 };
+    char.to_digit(36).unwrap() - 9 + start_value
+}
+
 pub fn part_1(input: String) -> u32 {
     input
         .lines()
         .map(|sack| sack.split_at(sack.len() / 2))
         .map(|(first, second)| first.chars().find(|char| second.contains(*char)).unwrap())
-        .map(|char| {
-            let start_value = if char.is_lowercase() { 0 } else { 26 };
-            char.to_digit(36).unwrap() - 9 + start_value
+        .map(|char| char_to_value(char))
+        .sum()
+}
+
+pub fn part_2(input: String) -> u32 {
+    input
+        .lines()
+        .tuples::<(_, _, _)>()
+        .map(|(first, second, third)| {
+            first
+                .chars()
+                .find(|char| second.contains(*char) && third.contains(*char))
+                .map(|char| char_to_value(char))
+                .unwrap()
         })
         .sum()
 }
@@ -23,5 +41,10 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
     #[test]
     fn priority_sum() {
         assert_eq!(part_1(INPUT.to_string()), 157);
+    }
+
+    #[test]
+    fn badge_sum() {
+        assert_eq!(part_2(INPUT.to_string()), 70);
     }
 }

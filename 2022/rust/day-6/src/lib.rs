ddb1c63f8589e178ec934bb8c2b8873db196090b
SinshiSmith <yorfnd@gmail.com>
2022-12-06 16:00:49 +0200

    day-6: solve part-2

--- 2022/rust/day-6/src/lib.rs
+++ 2022/rust/day-6/src/lib.rs
@@ -20,6 +20,22 @@ pub fn part_1(input: &str) -> u32 {
     chars.next().unwrap().0 as u32
 }
 
+pub fn part_2(input: &str) -> u32 {
+    let mut tracker = VecDeque::new();
+    let mut chars = input.chars().enumerate();
+    while tracker.len() < 14 {
+        let (_, current) = chars.next().unwrap();
+        if !tracker.contains(&current) {
+            tracker.push_back(current);
+        } else {
+            let first_appear = tracker.iter().position(|char| *char == current).unwrap();
+            drop(tracker.drain(..=first_appear));
+            tracker.push_back(current);
+        }
+    }
+    chars.next().unwrap().0 as u32
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -29,5 +45,9 @@ mod tests {
     #[test]
     fn processed_count() {
         assert_eq!(part_1(INPUT), 11);
+    }
+    #[test]
+    fn message_processed_count() {
+        assert_eq!(part_2(INPUT), 26);
     }
 }

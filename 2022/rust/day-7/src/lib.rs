80c906b27a9b5046f8dbf0fcfb1e00b7c257504a
SinshiSmith <yorfnd@gmail.com>
2022-12-07 22:22:11 +0200

    day-7: solve part-2

--- 2022/rust/day-7/src/lib.rs
+++ 2022/rust/day-7/src/lib.rs
@@ -104,16 +104,81 @@ pub fn part_1(input: String) -> u64 {
                 .push(File::from(line));
         }
     }
-    dbg!(&folders_map
-        .iter()
-        .map(|(key, folder)| (key, &folder.name, &folder.contains))
-        .collect::<Vec<_>>());
 
     folders_map
         .iter()
         .map(|(_, folder)| folder.size(&folders_map))
         .filter(|size| *size <= MAX_SIZE)
         .sum()
+}
+
+const TOTAL_SPACE: u64 = 70000000;
+const REQUIRED_SPACE: u64 = 30000000;
+
+pub fn part_2(input: String) -> u64 {
+    let mut folders_map: HashMap<String, Folder> = HashMap::new();
+    let mut current_path: Vec<&str> = vec!["/"];
+
+    for line in input.lines() {
+        if line.contains("$ cd") {
+            let path = line.split(" ").last().unwrap();
+            match path {
+                ".." => {
+                    current_path.pop();
+                }
+                "/" => {
+                    current_path = vec!["/"];
+                }
+                _ => {
+                    current_path.push(path);
+                }
+            }
+        }
+
+        if line.starts_with("dir") {
+            let folder = Folder::from(line);
+            let parent = current_path.iter().rev().next().unwrap();
+            folders_map
+                .entry(current_path.join(""))
+                .or_insert(Folder {
+                    name: &parent,
+                    files: vec![],
+                    contains: vec![],
+                })
+                .contains
+                .push(current_path.join("") + &folder.name);
+
+            folders_map.insert(current_path.join("") + &folder.name, folder);
+        }
+        if line.starts_with(|c: char| c.is_digit(10)) {
+            let folder = current_path.iter().last().unwrap();
+            folders_map
+                .entry(current_path.join(""))
+                .or_insert(Folder {
+                    name: folder,
+                    files: vec![],
+                    contains: vec![],
+                })
+                .files
+                .push(File::from(line));
+        }
+    }
+
+    let used_space = folders_map.get("/").unwrap().size(&folders_map);
+    let free_space = TOTAL_SPACE - used_space;
+    let needed_space = REQUIRED_SPACE - free_space;
+
+    folders_map
+        .iter()
+        .filter_map(|(_, folder)| {
+            let folder_size = folder.size(&folders_map);
+            if folder_size > needed_space {
+                return Some(folder_size);
+            }
+            None
+        })
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -147,5 +212,10 @@ $ ls
     #[test]
     fn largest_files_combined() {
         assert_eq!(part_1(INPUT.to_string()), 95437);
+    }
+
+    #[test]
+    fn smallest_file_to_free_space() {
+        assert_eq!(part_2(INPUT.to_string()), 24933642);
     }
 }

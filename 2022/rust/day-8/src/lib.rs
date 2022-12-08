b3dafecba62845c0bc3fd9a65866434314450ab5
SinshiSmith <yorfnd@gmail.com>
2022-12-08 17:54:06 +0200

    day-8: refactor part-2

--- 2022/rust/day-8/src/lib.rs
+++ 2022/rust/day-8/src/lib.rs
@@ -50,15 +50,17 @@ pub fn part_1(input: String) -> usize {
     visible
 }
 
-fn visible_trees(tree: &&u32, trees: &[&u32]) -> usize {
-    match trees
-        .iter()
-        .enumerate()
-        .find(|(_, visible)| *visible >= tree)
-    {
-        Some((idx, _)) => idx + 1,
-        None => trees.iter().len(),
+fn visible_trees<'a>(tree: &u32, trees: impl Iterator<Item = &'a u32>) -> usize {
+    let mut last_idx: usize = 0;
+
+    for (idx, height) in trees.enumerate() {
+        if height >= tree {
+            return idx + 1;
+        }
+        last_idx = idx;
     }
+
+    last_idx + 1
 }
 
 pub fn part_2(input: String) -> usize {
@@ -70,20 +72,17 @@ pub fn part_2(input: String) -> usize {
         for col in 1..(cols - 1) {
             let tree = grid.get(row, col).unwrap();
 
-            let mut current_row = grid.iter_row(row).collect::<Vec<&u32>>();
-            let mut current_col = grid.iter_col(col).collect::<Vec<&u32>>();
-
-            let (left, right) = current_row.split_at_mut(col);
-            let (up, down) = current_col.split_at_mut(row);
-
-            (*left).reverse();
-            (*up).reverse();
+            let up = grid.iter_col(col).take(row).rev();
+            let down = grid.iter_col(col).skip(row + 1);
+            let left = grid.iter_row(row).take(col).rev();
+            let right = grid.iter_row(row).skip(col + 1);
 
             let up_score = visible_trees(&tree, up);
-            let down_score = visible_trees(&tree, down.split_first().unwrap().1);
+            let down_score = visible_trees(&tree, down);
             let left_score = visible_trees(&tree, left);
-            let right_score = visible_trees(&tree, right.split_first().unwrap().1);
+            let right_score = visible_trees(&tree, right);
             let total_score = up_score * down_score * left_score * right_score;
+
             if total_score > score {
                 score = total_score
             }

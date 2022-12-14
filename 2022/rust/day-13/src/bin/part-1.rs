f36ffde838330e4fa25b8946aebae4604fc93f27
SinshiSmith <yorfnd@gmail.com>
2022-12-14 23:17:20 +0200

    day-13: solve part-1

---
+++ 2022/rust/day-13/src/bin/part-1.rs
@@ -0,0 +1,8 @@
+use std::fs::read_to_string;
+
+use day_13::part_1;
+
+fn main() {
+    let input = read_to_string("input.txt").unwrap();
+    println!("{:?}", part_1(&input));
+}
--- 2022/rust/day-13/src/lib.rs
+++ 2022/rust/day-13/src/lib.rs
@@ -1,14 +1,186 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{self, newline},
+    multi::{separated_list0, separated_list1},
+    sequence::{delimited, separated_pair},
+    IResult, Parser,
+};
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum SignalItem {
+    Number(u8),
+    List(Vec<SignalItem>),
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct SignalPart {
+    items: Vec<SignalItem>,
+}
+
+#[derive(Debug)]
+struct Pair {
+    left: SignalPart,
+    right: SignalPart,
+}
+
+fn nested_list_parser(input: &str) -> IResult<&str, SignalItem> {
+    let (input, item) = delimited(
+        tag("["),
+        separated_list0(
+            tag(","),
+            alt((
+                complete::u8.map(|number| SignalItem::Number(number)),
+                nested_list_parser,
+            )),
+        )
+        .map(|list| SignalItem::List(list)),
+        tag("]"),
+    )(input)?;
+    Ok((input, item))
+}
+
+fn list_parser(input: &str) -> IResult<&str, SignalPart> {
+    let (input, _) = tag("[")(input)?;
+    if input.chars().next().unwrap() == ']' {
+        let (input, _) = tag("]")(input)?;
+        return Ok((input, SignalPart { items: vec![] }));
+    }
+    let (input, items) = separated_list1(
+        tag(","),
+        alt((
+            complete::u8.map(|d| SignalItem::Number(d)),
+            nested_list_parser,
+        )),
+    )(input)?;
+    let (input, _) = tag("]")(input)?;
+    Ok((input, SignalPart { items }))
+}
+
+fn pair_parser(input: &str) -> IResult<&str, Pair> {
+    let (input, (left, right)) = separated_pair(list_parser, newline, list_parser)(input)?;
+    Ok((input, Pair { left, right }))
+}
+
+fn input_parser(input: &str) -> Vec<Pair> {
+    separated_list1(tag("\n\n"), pair_parser)(input).unwrap().1
+}
+
+fn is_number_equal(left: &u8, right: &u8) -> Option<bool> {
+    if left == right {
+        None
+    } else {
+        Some(left < right)
+    }
+}
+
+fn is_ordered(left: Vec<&SignalItem>, right: Vec<&SignalItem>, is_inner: bool) -> Option<bool> {
+    let mut right_items = right.iter();
+    for left_item in &left {
+        match right_items.next() {
+            Some(right_item) => match (left_item, right_item) {
+                (SignalItem::Number(left_number), SignalItem::Number(right_number)) => {
+                    if let Some(equal) = is_number_equal(left_number, right_number) {
+                        return Some(equal);
+                    } else {
+                        continue;
+                    }
+                }
+                (SignalItem::Number(_), SignalItem::List(right_list)) => {
+                    if let Some(equal) = is_ordered(
+                        vec![left_item],
+                        right_list.iter().collect::<Vec<&SignalItem>>(),
+                        true,
+                    ) {
+                        return Some(equal);
+                    } else {
+                        continue;
+                    }
+                }
+                (SignalItem::List(left_list), SignalItem::Number(_)) => {
+                    if let Some(equal) = is_ordered(
+                        left_list.iter().collect::<Vec<&SignalItem>>(),
+                        vec![right_item],
+                        true,
+                    ) {
+                        return Some(equal);
+                    } else {
+                        continue;
+                    }
+                }
+                (SignalItem::List(left_list), SignalItem::List(right_list)) => {
+                    if let Some(equal) = is_ordered(
+                        left_list.iter().collect::<Vec<&SignalItem>>(),
+                        right_list.iter().collect::<Vec<&SignalItem>>(),
+                        true,
+                    ) {
+                        return Some(equal);
+                    } else {
+                        continue;
+                    }
+                }
+            },
+            None => return Some(false),
+        }
+    }
+
+    if is_inner && left.len() == right.len() {
+        return None;
+    }
+
+    Some(left.len() < right.len())
+}
+
+pub fn part_1(input: &str) -> usize {
+    let pairs = input_parser(input);
+    pairs
+        .iter()
+        .enumerate()
+        .filter_map(|(idx, pair)| {
+            let left = pair.left.items.iter().collect::<Vec<&SignalItem>>();
+            let right = pair.right.items.iter().collect::<Vec<&SignalItem>>();
+            if is_ordered(left, right, false).unwrap() {
+                Some(idx + 1)
+            } else {
+                None
+            }
+        })
+        .inspect(|idx| {
+            dbg!(idx);
+        })
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INPUT: &str = "[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]";
+
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(part_1(INPUT), 13);
     }
 }
